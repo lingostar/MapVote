70e5d80834e71ee1377f1be67b00e1cec5838989
lingostar <lingostar.joker@gmail.com>
2021-02-24 22:54:18 +0900

    #3 데이터 모델링 #4 더미데이터 추가하고 테스트
    #3 데이터 모델링
    Category 스트럭처와 Item 스트럭처 그리고 Annotation 클래스 제작
    #4 더미데이터 추가하고 테스트
    Category내에 Item을 넣는 계층 구조 제작.
    맵 뷰에 추가하기 위해 Annotation으로 변환작업.

--- Application/MapVote/MapVote/MapViewController.swift
+++ Application/MapVote/MapVote/MapViewController.swift
@@ -13,7 +13,6 @@ class MapViewController: UIViewController {
     @IBOutlet weak var mainMapView: MKMapView!
     
     var region : MKCoordinateRegion?
-    private var allAnnotations: [MKAnnotation] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +22,9 @@ class MapViewController: UIViewController {
 
     //기본 위도, 경도 location으로 mapView 표시
     func setMapView(){
+        mainMapView.delegate = self
+        mainMapView.register(MKAnnotationView.self, forAnnotationViewWithReuseIdentifier: NSStringFromClass(Annotation.self))
+        
         // Set initial region with initialLocation and span
         let initialLocation = CLLocationCoordinate2D(latitude: 36.505733, longitude: 127.255664)
         let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.04)
@@ -32,14 +34,37 @@ class MapViewController: UIViewController {
         // setRegion on mapView
         self.mainMapView.setRegion(region!, animated: true)
         
-        // Set Annotation on initialLocation
-        self.mainMapView.addAnnotations(dummyAnnotations)
+        // Annotation 추가하기. flatMap으로 3개 카테고리 내의 item들을 하나의 array로 만들고,
+        // item들을 Annotation으로 변환
+        self.mainMapView.addAnnotations(dummyCategories.flatMap{$0.items}.map{$0.makeAnnotation()})
     }
-
-    // MARK: - Annotation
     
-    private func registerMapAnnotationViews() {
-        mainMapView.register(MKAnnotationView.self, forAnnotationViewWithReuseIdentifier: NSStringFromClass(Annotation.self))
+}
+
+extension MapViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !annotation.isKind(of: MKUserLocation.self) else {
+            // 사용자의 현재 위치를 나타내는 이미지는 커스터마이즈 하지 않는다.
+            return nil
+        }
+        
+        var annotationView: MKAnnotationView?
+        
+        if let annotation = annotation as? Annotation {
+            annotationView = setupAnnotationView(for: annotation, on: mapView)
+        }
+        
+        return annotationView
+    }
+    
+    private func setupAnnotationView(for annotation: Annotation, on mapView: MKMapView) -> MKAnnotationView {
+        let annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: NSStringFromClass(Annotation.self), for: annotation)
+        annotationView.canShowCallout = true
+        annotationView.calloutOffset = CGPoint(x: -5, y: 5)
+        
+        annotationView.image = UIImage(named: annotation.item.pinImageUrl)
+
+        return annotationView
     }
     
 }
--- Application/MapVote/MapVote/VoteModel.swift
+++ Application/MapVote/MapVote/VoteModel.swift
@@ -8,29 +8,93 @@
 import Foundation
 import MapKit
 
-let dummyAnnotations = createDummyAnnotations()
+let dummyCategories:[Category] = createCategoryDummyData()
 
-func createDummyAnnotations() -> [MKPointAnnotation] {
-    let annotation1 = MKPointAnnotation()
-    annotation1.coordinate = CLLocationCoordinate2D(latitude: 36.5085153116395, longitude: 127.25339880040323)
-    let annotation2 = MKPointAnnotation()
-    annotation2.coordinate = CLLocationCoordinate2D(latitude: 36.51154098158622, longitude: 127.24874279646995)
-    let annotation3 = MKPointAnnotation()
-    annotation3.coordinate = CLLocationCoordinate2D(latitude: 36.507612895951, longitude: 127.2421385355717)
-    let annotation4 = MKPointAnnotation()
-    annotation4.coordinate = CLLocationCoordinate2D(latitude: 36.51509731960058, longitude: 127.2649232356707)
-    let annotation5 = MKPointAnnotation()
-    annotation5.coordinate = CLLocationCoordinate2D(latitude: 36.50182656892, longitude: 127.26165412652605)
-    let annotation6 = MKPointAnnotation()
-    annotation6.coordinate = CLLocationCoordinate2D(latitude: 36.514221520182, longitude: 127.24649734776456)
-    let annotation7 = MKPointAnnotation()
-    annotation7.coordinate = CLLocationCoordinate2D(latitude: 36.5006320897654, longitude: 127.25184679909215)
-    let annotation8 = MKPointAnnotation()
-    annotation8.coordinate = CLLocationCoordinate2D(latitude: 36.51315993184142, longitude: 127.2365249138082)
-    let annotation9 = MKPointAnnotation()
-    annotation9.coordinate = CLLocationCoordinate2D(latitude: 36.509736210237556, longitude: 127.22866584333926)
-    let annotation10 = MKPointAnnotation()
-    annotation10.coordinate = CLLocationCoordinate2D(latitude: 36.50859493604809, longitude: 127.26730076959407)
-    
-    return [annotation1, annotation2, annotation3, annotation4, annotation5, annotation6, annotation7, annotation8, annotation9, annotation10]
+struct Category: Codable {
+    let categoryName: String
+    var items: [Item]
 }
+
+struct Item : Codable {
+    
+    let itemName: String
+    let pinImageUrl: String
+    let latitude: Double
+    let longitude : Double
+    
+    func makeAnnotation() -> Annotation {
+        return Annotation(item: self)
+    }
+}
+
+class Annotation: NSObject, MKAnnotation {
+    
+    @objc dynamic var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: item.latitude, longitude: item.longitude)
+    }
+    
+    var title: String? {
+        return item.itemName
+    }
+    
+    let item:Item
+    
+    init(item:Item) {
+        self.item = item
+    }
+}
+
+
+func createCategoryDummyData() -> [Category]{
+    
+    let politicsData : [Item] = [
+        Item(itemName: "민주당", pinImageUrl: "theminjoo_logo", latitude: 36.5085153116395, longitude: 127.25339880040323),
+        Item(itemName: "민주당", pinImageUrl: "theminjoo_logo", latitude: 36.51154098158622, longitude: 127.24874279646995),
+        Item(itemName: "민주당", pinImageUrl: "theminjoo_logo", latitude: 36.51509731960058, longitude: 127.2421385355717),
+        Item(itemName: "국민의힘", pinImageUrl: "peoplepowerparty_logo", latitude: 36.51509731960058, longitude: 127.2421385355717),
+        Item(itemName: "국민의힘", pinImageUrl: "peoplepowerparty_logo", latitude: 36.50182656892, longitude: 127.26165412652605),
+        Item(itemName: "국민의힘", pinImageUrl: "peoplepowerparty_logo", latitude: 36.514221520182, longitude: 127.24649734776456),
+        Item(itemName: "열린민주당", pinImageUrl: "openminjoo_logo", latitude: 36.5006320897654, longitude: 127.25184679909215),
+        Item(itemName: "열린민주당", pinImageUrl: "openminjoo_logo", latitude: 36.51315993184142, longitude: 127.2365249138082),
+        Item(itemName: "정의당", pinImageUrl: "justice_logo", latitude: 36.509736210237556, longitude: 127.22866584333926),
+        Item(itemName: "정의당", pinImageUrl: "justice_logo", latitude: 36.50859493604809, longitude: 127.26730076959407)]
+    
+    
+    
+    let hobbyData : [Item] = [
+        Item(itemName: "reading", pinImageUrl: "reading_logo", latitude: 36.51664006987988, longitude: 127.26384647265553),
+        Item(itemName: "reading", pinImageUrl: "reading_logo", latitude: 36.50608529449449, longitude: 127.25157268467761),
+        Item(itemName: "reading", pinImageUrl: "reading_logo", latitude: 36.51122489106196, longitude: 127.23736770663983),
+        Item(itemName: "soccer", pinImageUrl: "soccer_logo", latitude: 36.513294430884336, longitude: 127.2370243839078),
+        Item(itemName: "soccer", pinImageUrl: "soccer_logo", latitude: 36.513294430884336, longitude: 127.2370243839078),
+        Item(itemName: "soccer", pinImageUrl: "soccer_logo", latitude: 36.51346689003826, longitude: 127.25560672677898),
+        Item(itemName: "soccer", pinImageUrl: "soccer_logo", latitude: 36.52002005311437, longitude: 127.24904067952889),
+        Item(itemName: "piano", pinImageUrl: "piano_logo", latitude: 36.51101793403611, longitude: 127.2689963133282),
+        Item(itemName: "piano", pinImageUrl: "piano_logo", latitude: 36.50204926471498, longitude: 127.26745136103405),
+        Item(itemName: "baseball", pinImageUrl: "baseball_logo", latitude: 36.50601630430007, longitude: 127.26633556215492),
+        Item(itemName: "baseball", pinImageUrl: "baseball_logo", latitude: 36.501428318381606, longitude: 127.24852569543086),
+        Item(itemName: "climbing", pinImageUrl: "climbing_logo", latitude: 36.497978525847174, longitude: 127.25556381143748),
+        Item(itemName: "climbing", pinImageUrl: "climbing_logo", latitude: 36.51508798730051, longitude: 127.24904067952889),
+        Item(itemName: "biking", pinImageUrl: "biking_logo", latitude: 36.50618877967086, longitude: 127.26174362061403),
+        Item(itemName: "biking", pinImageUrl: "biking_logo", latitude: 36.508120477570024, longitude: 127.23625190776072)]
+    
+
+    let sellerData : [Item] = [
+        Item(itemName: "trumpet", pinImageUrl: "trumpet_logo", latitude: 36.509569219360635, longitude: 127.23110206678025),
+        Item(itemName: "trumpet", pinImageUrl: "trumpet_logo", latitude: 36.51191474381813, longitude: 127.22934253777862),
+        Item(itemName: "trumpet", pinImageUrl: "trumpet_logo", latitude: 36.510224593643926, longitude: 127.26749427637556),
+        Item(itemName: "radio", pinImageUrl: "radio_logo", latitude: 36.5029116819165, longitude: 127.27101333437888),
+        Item(itemName: "radio", pinImageUrl: "radio_logo", latitude: 36.5106385113848, longitude: 127.2637606416647),
+        Item(itemName: "radio", pinImageUrl: "radio_logo", latitude: 36.50636125465736, longitude: 127.24414833059743),
+        Item(itemName: "shoes", pinImageUrl: "shoes_logo", latitude: 36.50198027092386, longitude: 127.23539360093064),
+        Item(itemName: "shoes", pinImageUrl: "shoes_logo", latitude: 36.509845167103634, longitude: 127.25560672677898),
+        Item(itemName: "pants", pinImageUrl: "pants_logo", latitude: 36.51646761779719, longitude: 127.2669792922775),
+        Item(itemName: "pants", pinImageUrl: "pants_logo", latitude: 36.49390757297461, longitude: 127.26496227122682)]
+    
+    let politicsCategory = Category(categoryName: "politics", items: politicsData)
+    let hobbyCategory = Category(categoryName: "hobby", items: hobbyData)
+    let sellerCategory = Category(categoryName: "seller", items: sellerData)
+
+    return [politicsCategory, hobbyCategory, sellerCategory]
+}
+
