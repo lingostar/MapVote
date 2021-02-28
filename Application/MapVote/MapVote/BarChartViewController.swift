7a6f64bf6f77a06351f4a6253d8f52eb2d128281
lingostar <lingostar.joker@gmail.com>
2021-02-28 21:27:04 +0900

    #9 Pod 와 SwiftPackage
    CocoaPod 를 통한 Chart 추가
    Swift Package Manager 를 이용한 Chart 추가

---
+++ Application/MapVote/MapVote/BarChartViewController.swift
@@ -0,0 +1,32 @@
+//
+//  BarChartViewController.swift
+//  MapVote
+//
+//  Created by LingoStar on 2021/02/28.
+//
+
+import UIKit
+import Charts
+
+class BarChartViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let barChartView = BarChartView()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
